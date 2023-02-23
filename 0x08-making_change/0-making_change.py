67bd95a63eba1a1eb1c47f833018351ed5f2606c
Ifeanyi <jax64720@gmail.com>
2023-02-23 11:24:55 +0000

    Making change script

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def makeChange(coins, total):
+    if 0 > total or total == 0:
+        return -1
+    coin_count = 1
+    max_value = max(coins)
+    current_sum = max_value
+    while current_sum < total:
+        if (current_sum + max_value) <= total:
+            current_sum += max_value
+            coin_count += 1
+        else:
+            coins.remove(max_value)
+            if coins != []:
+                max_value = max(coins)
+            else:
+                break
+    if current_sum >= total:
+        return coin_count
+    else:
+        return -1
