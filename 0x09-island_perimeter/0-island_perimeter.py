a28075a0635c6ce359ef76cd2d9dad7fe0772fa3
Ifeanyik <99406152+Ifeanyik@users.noreply.github.com>
2023-03-04 18:00:56 +0100

    Manage edge cases

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,23 +8,20 @@ def island_perimeter(grid):
     '''This function finds the perimeter of
        a given island with its shape represented by 1's
        in a list'''
-    if grid == [] or type(grid) != list:
-        return 0
-    list_len = len(grid)
-    row_len = len(grid[0])
     perimeter = 0
-    for row in range(list_len):
-        for elem in range(row_len):
-            if grid[row][elem]:
-                try:
-                    if not grid[row - 1][elem]:
-                        perimeter += 1
-                    if not grid[row][elem - 1]:
-                        perimeter += 1
-                    if not grid[row][elem + 1]:
-                        perimeter += 1
-                    if not grid[row + 1][elem]:
-                        perimeter += 1
-                except IndexError:
-                    pass
-    return perimeter
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
+    return 
